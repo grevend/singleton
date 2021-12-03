bfabb190957bf9506cb4b8d2a1a0d67701908e18
duart38 <duartsnel@icloud.com>
2021-12-03 09:20:59 -0400

    bundled

--- singleton.bundle.js
+++ singleton.bundle.js
@@ -2,6 +2,8 @@ function singleton(factory) {
     let instance = null;
     return {
         getInstance: ()=>instance == null ? instance = factory() : instance
+        ,
+        setInstance: (x)=>instance = x
     };
 }
 export { singleton as default };
